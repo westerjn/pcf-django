4f9c6524c2e236a123afc9134bd648d3637e1443
Jonathon <jnw777@yahoo.com>
2020-10-06 11:53:35 -0400

    Add files via upload

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.urls import path
 from . import views
-
+from warapp import urls
 
 urlpatterns = [
+    # path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('war/', views.warindex, name='warindex'),
+    path('six/', views.pageSix, name='game'),
 ]
--- app/views.py
+++ app/views.py
@@ -8,3 +8,22 @@ def index(request):
     Function that shows the hello world with an Http Response.
     """
     return HttpResponse("Hello World, This is a Django App that has been deployed on Cloud Foundry 😄")
+
+def warindex(request):
+    return HttpResponse("Welcome to Wartown")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+def pageSix(request):
+    mando = 'westy'
+    context = {'latest_question_list': mando}
+    return render(request, 'index.html', context)
+
